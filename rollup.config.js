d7be6746c7dbc5b29d4c1c75792698ecc020080f
mikob <fxrock2002@yahoo.com>
2019-07-31 01:27:48 +0300

    Allow changing the output via the cli.

--- rollup.config.js
+++ rollup.config.js
@@ -15,136 +15,138 @@ const FOLDER_REGX = /^src\/(.*)\/.*$/;
 const PLANS = ['0', '10', '20'];
 const PLANS_AND_PARTS = ['backend', ...[].concat.apply([], ['matching.cs', 'nonmatching.cs'].map(cs => PLANS.map(plan => `${plan}.${cs}`)))];
 
-module.exports = [].concat(...globby.sync(['src/*/*.ts', '!src/*/tests.ts', '!src/@types', '!src/*/*.*.ts']).map(fileName => {
-	console.log('doing ' + fileName)
-	let regxRes = FOLDER_REGX.exec(fileName);
-	let folderName = regxRes ? regxRes[1] : null;
-	if (folderName) {
-		return [
-			{
-				input: `src/${folderName}/*.ts`,
-				treeshake: false,
-				plugins: [
-					typescript(),
-					multiEntry(),
-				],
-				output: {
-					// garbage since we use jscodeshift on the cmd line
-					file: `dist/tmp/${folderName}.joined.mjs`,
-					format: 'esm',
-				}
-			},
-			{
-				input: `dist/tmp/${folderName}.joined.mjs`,
-				treeshake: false,
-				plugins: [
-					{
-						/**
-						 * Trim down for matching and non-matching URL frontend CS
-						 */
-						generateBundle(options, bundle, isWrite) {
-							if (isWrite) {
-								// async/await syntax messes shiz up
-								return new Promise(async cb => {
-									// make this the node_modules path instead
-									let res = await JSCodeShift('/home/mikob/workspace/lipsurf/lipsurf-cli/transforms/split.ts', [`dist/tmp/${folderName}.joined.mjs`], {
-										transform: './node_modules/lipsurf-cli/transforms/split.ts',
-										verbose: 2,
-										runInBand: true,
+module.exports = function getConfig(finalOutputDir) {
+	return [].concat(...globby.sync(['src/*/*.ts', '!src/*/tests.ts', '!src/@types', '!src/*/*.*.ts']).map(fileName => {
+		console.log('doing ' + fileName)
+		let regxRes = FOLDER_REGX.exec(fileName);
+		let folderName = regxRes ? regxRes[1] : null;
+		if (folderName) {
+			return [
+				{
+					input: `src/${folderName}/*.ts`,
+					treeshake: false,
+					plugins: [
+						typescript(),
+						multiEntry(),
+					],
+					output: {
+						// garbage since we use jscodeshift on the cmd line
+						file: `dist/tmp/${folderName}.joined.mjs`,
+						format: 'esm',
+					}
+				},
+				{
+					input: `dist/tmp/${folderName}.joined.mjs`,
+					treeshake: false,
+					plugins: [
+						{
+							/**
+							 * Trim down for matching and non-matching URL frontend CS
+							 */
+							generateBundle(options, bundle, isWrite) {
+								if (isWrite) {
+									// async/await syntax messes shiz up
+									return new Promise(async cb => {
+										// make this the node_modules path instead
+										let res = await JSCodeShift('/home/mikob/workspace/lipsurf/lipsurf-cli/transforms/split.ts', [`dist/tmp/${folderName}.joined.mjs`], {
+											transform: './node_modules/lipsurf-cli/transforms/split.ts',
+											verbose: 2,
+											runInBand: true,
 
-										dry: false,
-										print: false,
-										babel: true,
-										extensions: 'js,mjs',
-										ignorePattern: [],
-										ignoreConfig: [],
-										silent: false,
-										parser: 'babel',
-										stdin: false
+											dry: false,
+											print: false,
+											babel: true,
+											extensions: 'js,mjs',
+											ignorePattern: [],
+											ignoreConfig: [],
+											silent: false,
+											parser: 'babel',
+											stdin: false
+										});
+										
+										for (let planAndPart of PLANS_AND_PARTS) {
+											const pattern = `dist/tmp/${folderName}.*.${planAndPart}.js`;
+											const fileParts = globby.sync(pattern);
+											if (fileParts.length !== 1) {
+												console.error(`Not exactly one file for ${folderName}`);
+												console.log(fileParts);
+												return;
+											}
+											const filePartName = fileParts[0].split('dist/tmp/')[1];
+											try {
+												const source = await fs.readFileSync(`dist/tmp/${filePartName}`);
+												bundle[filePartName] = {
+													isAsset: true,
+													fileName: filePartName,
+													source,
+												};
+											} catch (e) {
+												if (e.code !== 'ENOENT')
+													console.log(`${filePartName} problem`, e);
+											}
+										}
+										cb();
 									});
-									
-									for (let planAndPart of PLANS_AND_PARTS) {
-										const pattern = `dist/tmp/${folderName}.*.${planAndPart}.js`;
-										const fileParts = globby.sync(pattern);
-										if (fileParts.length !== 1) {
-											console.error(`Not exactly one file for ${folderName}`);
-											console.log(fileParts);
-											return;
-										}
-										const filePartName = fileParts[0].split('dist/tmp/')[1];
-										try {
-											const source = await fs.readFileSync(`dist/tmp/${filePartName}`);
-											bundle[filePartName] = {
-												isAsset: true,
-												fileName: filePartName,
-												source,
-											};
-										} catch (e) {
-											if (e.code !== 'ENOENT')
-												console.log(`${filePartName} problem`, e);
-										}
-									}
-									cb();
-								});
+								}
 							}
 						}
+					],
+					output: {
+						// garbage since we use jscodeshift on the cmd line
+						file: `/tmp/${folderName}.garbage.js`,
+						format: 'esm',
 					}
-				],
-				output: {
-					// garbage since we use jscodeshift on the cmd line
-					file: `/tmp/${folderName}.garbage.js`,
-					format: 'esm',
-				}
-			},
-			// to prevent chunking external deps, do the files one by one :( (rollup shortcoming)
-			// hack: manually including version
-			...PLANS_AND_PARTS.map(planAndPart => `dist/tmp/${folderName}.2-0-0.${planAndPart}.js`).map(filename => ({
-				// don't use globby.sync because it resolves before files are ready
-				input: filename,
-				treeshake: {
-					moduleSideEffects: false,
-					pureExternalModules: true,
 				},
-				plugins: [
-					resolve(),
-					commonjs(),
-					html({
-						include: '**/*.html',
-						htmlMinifierOptions: {
-							collapseWhitespace: true,
-							collapseBooleanAttributes: true,
-							conservativeCollapse: true,
-							minifyJS: true,
-							minifyCSS: true,
-							removeComments: true,
-						}
-					}),
-					PROD && terser({
-						mangle: false,
-						compress: {
-							pure_funcs: [
-								'console.log',
-								'console.dir',
-							]
-						}
-					}),
-				],
-				output: {
-					format: 'esm',
-					file: `${filename.split('.js')[0]}.resolved.js`
+				// to prevent chunking external deps, do the files one by one :( (rollup shortcoming)
+				// hack: manually including version
+				...PLANS_AND_PARTS.map(planAndPart => `dist/tmp/${folderName}.2-3-0.${planAndPart}.js`).map(filename => ({
+					// don't use globby.sync because it resolves before files are ready
+					input: filename,
+					treeshake: {
+						moduleSideEffects: false,
+						pureExternalModules: true,
+					},
+					plugins: [
+						resolve(),
+						commonjs(),
+						html({
+							include: '**/*.html',
+							htmlMinifierOptions: {
+								collapseWhitespace: true,
+								collapseBooleanAttributes: true,
+								conservativeCollapse: true,
+								minifyJS: true,
+								minifyCSS: true,
+								removeComments: true,
+							}
+						}),
+						PROD && terser({
+							mangle: false,
+							compress: {
+								pure_funcs: [
+									'console.log',
+									'console.dir',
+								]
+							}
+						}),
+					],
+					output: {
+						format: 'esm',
+						file: `${filename.split('.js')[0]}.resolved.js`
+					}
+				})),
+				{
+					// hack, manually including version in the input file name
+					input: PLANS_AND_PARTS.map(planAndPart => `dist/tmp/${folderName}.2-3-0.${planAndPart}.resolved.js`),
+					plugins: [
+						makeCS(),
+					],
+					output: {
+						format: 'esm',
+						dir: finalOutputDir,
+					}
 				}
-			})),
-			{
-				// hack, manually including version in the input file name
-				input: PLANS_AND_PARTS.map(planAndPart => `dist/tmp/${folderName}.2-0-0.${planAndPart}.resolved.js`),
-				plugins: [
-					makeCS(),
-				],
-				output: {
-					format: 'esm',
-					dir: 'dist',
-				}
-			}
-		];
-	}
-}).filter(a => a));
+			];
+		}
+	}).filter(a => a));
+}
